e3bacaafe75034bfb4199c3fa8aa5a386b0a18b9
Vishal Pancholi <vishal@Vishals-MacBook-Air.local>
2025-05-23 22:57:26 +0530

    added terraform scripts for azure

--- terraform/modules/security/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "security_rule_id" {
-    description = "The ID of the security group rule"
-    value       = aws_security_group_rule.eks_to_rds.id
-  }
--- terraform/modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "eks_node_security_group_id" {
-    description = "Security group ID of the EKS nodes"
-    type        = string
-  }
-  
-  variable "rds_security_group_id" {
-    description = "Security group ID of the RDS instance"
-    type        = string
-  }
